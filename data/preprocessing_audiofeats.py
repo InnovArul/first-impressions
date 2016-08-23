217d45f581fa188e72ae54695296192c01ef9978
Arul <arul.subramaniam@hotmail.com>
2016-08-23 23:48:53 +0530

    data processing scripts are added under data/" folder

---
+++ data/preprocessing_audiofeats.py
@@ -0,0 +1,77 @@
+import scipy.io.wavfile as wav
+import subprocess
+import os
+
+# collect all file names from train folder
+from os import listdir
+from os.path import isfile, join
+import exceptions
+import numpy as np
+import logging
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import audioFeatureExtraction
+
+rootpath = './train'
+basepath = rootpath
+
+##
+# to create the parent directory (recursively if the fiolder structure is not existing already)
+#
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if os.path.isdir(path):
+            pass
+        else:
+            raise
+
+##
+# extractWavFile - to extract the wav audio file from mp4 files using ffmpeg
+#
+def extractWavFile(filepath):
+    if(isfile(filepath)):
+        if(filepath.lower().endswith('.mp4')):
+            path, filename = os.path.split(filepath)
+            destpath = path.replace(basepath + '/', basepath + 'audio/')
+            mkdir_p(destpath)
+            command = "ffmpeg -i " + filepath + " -ab 160k -ac 2 -ar 44100 -vn " + join(destpath, filename) + ".wav"
+            subprocess.call(command, shell = True)
+    else:
+        allfiles = [f for f in listdir(filepath) if (f != '.' and f != '..')]
+        for anyfile in allfiles:
+            print join(filepath, anyfile)
+            extractWavFile(join(filepath, anyfile))
+
+extractWavFile(basepath)
+
+basepath = rootpath + 'audio'
+sizeArray = None
+
+##
+# extractMFCCFeatures - to extract audio features from .wav files using pyAudioAnalysis toolkit
+#         thanks to : https://github.com/tyiannak/pyAudioAnalysis
+#         the featrures extracted are, as in https://github.com/tyiannak/pyAudioAnalysis/wiki/3.-Feature-Extraction
+#
+def extractMFCCFeatures(filepath):
+    global sizeArray
+    if(isfile(filepath)):
+        if(filepath.lower().endswith('.wav')):
+            path, filename = os.path.split(filepath)
+            destpath = path.replace(basepath + '/', basepath + 'feat/')
+            mkdir_p(destpath)
+            (rate, sig) = audioBasicIO.readAudioFile(filepath);
+            command = "python pyAudioAnalysis/audioAnalysis.py featureExtractionFile -i " + filepath + " -mw " + str(sig.shape[0]/float(rate)/5.5) + " -ms " +  str(sig.shape[0]/float(rate)/5.5) + " -sw 0.050 -ss 0.050 -o " + join(destpath, filename)
+            print(command)
+            subprocess.call(command, shell = True)
+    else:
+        allfiles = [f for f in listdir(filepath) if (f != '.' and f != '..')]
+        for anyfile in allfiles:
+            extractMFCCFeatures(join(filepath, anyfile))
+            
+logging.basicConfig(filename=basepath+'_modifiedrate.log',level=logging.INFO)
+extractMFCCFeatures(basepath)
+
+# remove the .npy, .wav_st.csv files
+subprocess.call("rm -f " + rootpath + "audiofeat/*/*.npy", shell=True)
+subprocess.call("rm -f " + rootpath + "audiofeat/*/*.wav_st.csv", shell=True)
---
+++ data/preprocessing_videofeats.py
@@ -0,0 +1,47 @@
+import scipy.io.wavfile as wav
+import subprocess
+import os
+
+# collect all file names from train folder
+from os import listdir
+from os.path import isfile, join
+import exceptions
+import numpy as np
+import logging
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import audioFeatureExtraction
+
+rootpath = './train'
+basepath = rootpath
+OpenfaceBase = "./OpenFace-master/build/bin"
+
+##
+# to create the parent directory (recursively if the fiolder structure is not existing already)
+#
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if os.path.isdir(path):
+            pass
+        else:
+            raise
+
+# extract open face features
+def extractOPENFACEFeatures(filepath):
+    global sizeArray
+    if(os.path.isfile(filepath)):
+        if(filepath.lower().endswith('.mp4')):
+			destbasepath = rootpath + 'frames'
+            path, filename = os.path.split(filepath)
+            destpath = destbasepath + '/' + filename.replace(".mp4","")
+            mkdir_p(destpath)
+			command = OpenfaceBase + '/FeatureExtraction' + ' -f ' + filepath + ' -rigid' + ' -simalign ' + destpath + ' -no2Dfp -no3Dfp -noMparams -noPose -noAUs -noGaze' + ' -q';
+			print(command)
+			subprocess.call(command, shell = True)
+    else:
+        allfiles = [f for f in os.listdir(filepath) if (f != '.' and f != '..')]
+        for anyfile in allfiles:
+            extractOPENFACEFeatures(filepath + '/' + anyfile)
+
+extractOPENFACEFeatures(basepath)
